a6e9e50d32dc16541b42860daa6982b1afec946a
Thomas F. Düllmann <duellmann@iste.uni-stuttgart.de>
2022-04-26 16:43:26 +0200

    Added duration measurement and loading of the model.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
@@ -6,6 +7,8 @@ import tensorflow as tf
 
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+start_time = datetime.datetime.now()
 
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
@@ -17,17 +20,19 @@ model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=5)
+model.fit(x_train, y_train, epochs=100)
 
 loss, accuracy = model.evaluate(x_test, y_test)
 print(f'Accuracy: {accuracy}')
 print(f'Loss: {loss}')
+print(f'Duration: {datetime.datetime.now()-start_time}')
 
 model.save('digits.model')
-
+model = tf.keras.models.load_model('digits.model')
 
 for x in range(0, 10):
     img = cv.imread(f'digits/{x}.png')[:, :, 0]
+    # For further optimization in flexibility
     # if img not (28,28):
     #     img = cv.resize(img, (28, 28))
     img = np.invert(np.array([img]))
